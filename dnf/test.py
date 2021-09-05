d38aca0926ef452ec23c0783bf72094fc30154f5
zj911 <740213682@qq.com>
2021-09-06 01:59:17 +0800

    yolov5检测示例

--- dnf/test.py
+++ dnf/test.py
@@ -5,150 +5,114 @@ import win32com.client
 import win32gui
 import win32process
 import win32api
+import win32con
 
 
-
-# 键值表：https://www.win.tue.nl/~aeb/linux/kbd/scancodes-1.html
-# https://blog.csdn.net/deniece1/article/details/103588428?utm_medium=distribute.pc_relevant.none-task-blog-2~default~baidujs_baidulandingword~default-0.control&spm=1001.2101.3001.4242
-
-# KeyBoard Commands
-# Command port
-KBC_KEY_CMD = 0x64
-# Data port
-KBC_KEY_DATA = 0x60
-
-g_winio = None
-
-
-key_alpha = {
-    # 字母表
-    'q': 0x10,
-    'w': 0x11,
-    'e': 0x12,
-    'r': 0x13,
-    't': 0x14,
-    'y': 0x15,
-    'u': 0x16,
-    'i': 0x17,
-    'o': 0x18,
-    'p': 0x19,
-    'a': 0x1e,
-    's': 0x1f,
-    'd': 0x20,
-    'f': 0x21,
-    'g': 0x22,
-    'h': 0x23,
-    'j': 0x24,
-    'k': 0x25,
-    'l': 0x26,
-    'z': 0x2c,
-    'x': 0x2d,
-    'c': 0x2e,
-    'v': 0x2f,
-    'b': 0x30,
-    'n': 0x31,
-    'm': 0x32}
-key_beta = {
-    # 数字表
-    '0': 0x0b,
-    '1': 0x02,
-    '2': 0x03,
-    '3': 0x04,
-    '4': 0x05,
-    '5': 0x06,
-    '6': 0x07,
-    '7': 0x08,
-    '8': 0x09,
-    '9': 0x0a,
-}
-
-key_gamma = {
-    'Enter': 0x0a,
-    'Space': 0x0a,
-    'Esc': 0x01,
-    'F1': 0x3b,
-    'F2': 0x3c,
-    'F3': 0x3d,
-    'F4': 0x3e,
-    'F5': 0x3f,
-    'F6': 0x40,
-    'F7': 0x41,
-    'F8': 0x42,
-    'F9': 0x43,
-    'F10': 0x44,
-    'F11': 0x45,
-    'F12': 0x46,
-    'Up': 0x48,
-    'Down': 0x50,
-    'Left': 0x4b,
-    'Right': 0x4d,
-}
-
-def get_winio():
-    global g_winio
-    if g_winio is None:
-            g_winio = pywinio.WinIO()
-            def __clear_winio():
-                    global g_winio
-                    g_winio = None
-            atexit.register(__clear_winio)
-    return g_winio
+def person_locate(dm):
+    # 人物坐标计算
+    dm_ret = None
+    num = 0
+    while 1:
+        dm_ret = dm.FindStrFast(0, 0, 1920, 1080, "lv2", "d9d9d9-505050", 0.9)
+        if dm_ret[1] != -1:
+            break
+        else:
+            num += 1
+            print('计算当前人物坐标', num)
+            time.sleep(1)
+    dm = win32com.client.Dispatch('dm.dmsoft')
+    # 返回人物坐标 x = dm_ret[1] y = dm_ret[2]
+    return dm_ret[1], dm_ret[2], dm
 
 
-def wait_for_buffer_empty():
-    '''
-    Wait keyboard buffer empty
-    '''
-    winio = get_winio()
-    dwRegVal = 0x02
-    while (dwRegVal & 0x02):
-            dwRegVal = winio.get_port_byte(KBC_KEY_CMD)
+def move():
+    # x轴速度 217 y轴速度 281
+    pass
 
-
-def key_down(scancode):
-    winio = get_winio()
-    wait_for_buffer_empty()
-    winio.set_port_byte(KBC_KEY_CMD, 0xd2)
-    wait_for_buffer_empty()
-    winio.set_port_byte(KBC_KEY_DATA, scancode)
-
-
-def key_up(scancode):
-    winio = get_winio()
-    wait_for_buffer_empty()
-    winio.set_port_byte(KBC_KEY_CMD, 0xd2)
-    wait_for_buffer_empty()
-    winio.set_port_byte(KBC_KEY_DATA, scancode | 0x80)
-
-
-def key_press(scancode, press_time=0.2):
-    key_down(scancode)
-    time.sleep(press_time)
-    key_up(scancode)
 
 def dnf_run():
     # 运行前
     dm = win32com.client.Dispatch('dm.dmsoft')
     whandle = win32gui.FindWindow(None, '地下城与勇士')
-    print(whandle)
-    dm_ret = dm.SetWindowState(whandle, 1)
+    state = dm.SetKeypadDelay('windows', 1000)
+    dm.SetWindowState(whandle, 1)
+    dm.SetPath(r'D:\python_code\pydamo\接口说明')
+    dm.SetDict(0, "yunjisuan.txt")
+
     time.sleep(2)
-    print(2)
+    dm.MoveTo(1000, 0)
+    dm.LeftClick()
+    time.sleep(2)
+    # dm.KeyDown(39)
+    # dm.KeyUp(39)
+    x1, y1, dm = person_locate(dm)
+    print('1', x1, y1)
+    # time.sleep(1)
+    dm.KeyDown(37)
+    time.sleep(1)
+    dm.KeyUp(37)
+    time.sleep(2)
+    dm.KeyDown(38)
+    time.sleep(1)
+    dm.KeyUp(38)
+    # # # 人物 70 高度和物品
+    x2, y2, dm = person_locate(dm)
+    print('2', x2, y2)
+    speed_x = (x1 - x2)
+    speed_y = (y1 - y2)
+    print('3', speed_x, speed_y)
+
+    # print(2)
     # status = dm.BindWindowEx(whandle, 'normal', 'dx.mouse.position.lock.api', "dx.keypad.input.lock.api", "dx.public.active.api", 0)
     # 绑定窗口目前失败
+    # print('bind')
     # try:
-    #     status = dm.BindWindowEx(whandle, 'normal', 'normal', "normal", "dx.public.active.api", 0)
+    #     status = dm.BindWindow(whandle, 'gdi', 'dx', 'dx', 0)
+    #     print(status)
     # except Exception as e:
+    #     print(dm.GetLastError())
     #     print(e)
 
-
-    time.sleep(2)
-    dm.KeyDownChar('right')
-    time.sleep(2)
-    dm.KeyDownChar('left')
 
     # 运行后
     # 解除绑定
 
-time.sleep(3)
+    # 如何寻路 -- 速度 时间 路程
+
+
+def shuatu():
+    '''
+    1.得到现在所处的坐标
+    2.按指定的移动键，固定时间，再释放
+    3.重新取得人物坐标
+    4.现在人物坐标- 之前坐标 = 路程
+
+    大漠工具将运行目录置全局 -- 提取特征字库文件
+    大漠工具设置字库_置文件
+    大漠 找字 --
+    '''
+    dm = win32com.client.Dispatch('dm.dmsoft')
+    # whandle = win32gui.FindWindow(None, '地下城与勇士')
+    # dm_ret = dm.SetWindowState(whandle, 1)
+    dm_ret = dm.SetDict(0, "dm_soft.txt")
+    print(dm_ret)
+    time.sleep(2)
+    # 需要新建文件代表字库 使用默认文件报错 --
+    dm_ret = dm.FindStr(0, 0, 1280, 720, "淡忘", "bea74c-000000", 0.9)
+    print(dm_ret)
+
+# shuatu()
+
 dnf_run()
+# import os
+# time.sleep(3)
+# # dnf_run()
+# # shuatu()
+# dm = win32com.client.Dispatch('dm.dmsoft')
+# dm_ret = dm.SetPath(r'D:\python_code\pydamo\接口说明')
+# print(os.getcwd())
+# dm_ret = dm.SetDict(0, "yunjisuan.txt")
+# print(dm_ret)
+# time.sleep(2)
+# dm_ret = dm.FindStrFast(0, 0, 1920, 1080, "云计算", "dcffff-808080", 0.9)
+# print(dm_ret)
